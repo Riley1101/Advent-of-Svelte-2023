6aa86d82ac403dc213753f5ecfc5710c86eb3b42
Riley1101 <arkardev00@gmail.com>
2023-12-02 12:23:28 -0500

    update on title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,13 @@
 	import Header from '$lib/components/Header.svelte';
 </script>
 
+<svelte:head>
+	<!-- Primary Meta Tags -->
+	<title>Advent of Svelte 2023 Solutions</title>
+	<meta name="title" content="Advent of Svelte 2023 Solutions" />
+	<meta name="description" content="Advent of Svelte 2023 Solutions " />
+</svelte:head>
+
 <Header />
 <main class="font-mono">
 	<slot />
