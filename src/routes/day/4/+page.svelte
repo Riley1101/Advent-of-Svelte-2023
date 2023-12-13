0e10d367e669f37801568be75c94b36998d041b2
Riley1101 <arkardev00@gmail.com>
2023-12-13 13:53:50 -0500

    day 4 complete

--- src/routes/day/4/+page.svelte
+++ src/routes/day/4/+page.svelte
@@ -3,7 +3,6 @@
 	import * as d3 from 'd3';
 	let chartData: number[] = [100, 2, 20, 100, 38];
 
-	let isMonitoring = false;
 	let fetching = false;
 	let ctx: HTMLDivElement;
 
@@ -21,10 +20,7 @@
 			.duration(200)
 			.ease(d3.easeLinear)
 			.style('height', function (d) {
-				return d + 'px';
-			})
-			.text(function (d) {
-				return d;
+				return d + '%';
 			});
 	}
 
@@ -51,30 +47,30 @@
 
 <div class="mt-12 text-center">
 	<h1 class="text-4xl font-bold">Santa's Health Monitor</h1>
-	<div class="border rounded-md w-full h-[400px] mt-4 flex flex-row">
-		<div bind:this={ctx} class="chart"></div>
+	<div
+		class="border rounded-md w-full max-w-[100vw] relative h-[400px] mt-12 flex flex-row justify-end overflow-x-scroll"
+	>
+		<div bind:this={ctx} class="chart">
+			<span class="absolute w-full top-1/2 p-[1px] bg-red-500/20 -translate-y-1/2"></span>
+		</div>
 	</div>
-
-	<button
-		class="mt-4 px-4 py-2 border bg-secondary text-white rounded-md hover:scale-105 duration-200 max-w-max mb-4"
-		>{isMonitoring ? 'Monitoring' : 'Monitor'}
-	</button>
 </div>
 
 <style>
 	.chart {
-		max-width: 400px;
+		padding: 1em;
 		display: flex;
+		gap: 1px;
 		align-items: center;
-		overflow-x: auto;
+		position: relative;
 	}
 	.chart :global(div) {
 		max-width: max-content;
 		border-radius: 5px;
 		font: 10px sans-serif;
-		background-color: steelblue;
+		background-color: red;
 		text-align: right;
-		padding: 3px;
+		padding: 2px;
 		margin: 1px;
 		color: white;
 		transform-origin: center;
